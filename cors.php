72dd04384d894e3934e03311e6f90c1bbae6ce21
Tomofumi Inoue <funaox@gmail.com>
2026-03-25 23:28:46 +0900

    feat: Add support for forwarding request method, headers, and body to proxied cURL requests.

--- cors.php
+++ cors.php
@@ -20,6 +20,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
+function get_request_headers()
+{
+    $request_headers = [];
+    foreach (getallheaders() as $name => $value) {
+        if (strtolower($name) === 'host') {
+            continue;
+        }
+        $request_headers[] = "$name: $value";
+    }
+    return $request_headers;
+}
+
 function header_function($ch, $header)
 {
     $len = strlen($header);
@@ -56,11 +68,21 @@ function header_function($ch, $header)
     return $len;
 }
 
+
+error_log($_GET['url']);
 $ch = curl_init($_GET['url']);
 $stream = fopen('php://output', 'w');
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
+curl_setopt($ch, CURLOPT_HTTPHEADER, get_request_headers());
 curl_setopt($ch, CURLOPT_FILE, $stream);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_HEADERFUNCTION, 'header_function');
+
+$input = file_get_contents('php://input');
+if ($input) {
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
+}
 
 if (!curl_exec($ch)) {
     error_log('cURL Error: ' . curl_error($ch));
