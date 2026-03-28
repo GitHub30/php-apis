08549542623b5ffddf05e12688e78ca1c3871c75
GitHub30 <funaox@gmail.com>
2026-03-28 13:33:56 +0000

    fix: Ensure Content-Type header is set correctly in WHOIS response

--- whois.php
+++ whois.php
@@ -3,6 +3,7 @@ header('X-Accel-Buffering: no');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
+header('Content-Type: text/plain');
 
 $domain = $_GET['domain'] ?? '';
 
