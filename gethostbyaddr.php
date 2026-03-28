d1d9e075df9c558ddee8c9d37ed603c93fcad3fc
GitHub30 <funaox@gmail.com>
2026-03-28 13:39:47 +0000

    feat: Add input validation and response headers for gethostbyaddr and gethostbyname endpoints fix: Remove unnecessary headers from imap.php

--- gethostbyaddr.php
+++ gethostbyaddr.php
@@ -1,1 +1,11 @@
+<?php
+header('X-Accel-Buffering: no');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: text/plain');
 
+if (!isset($_GET['ip'])) {
+    http_response_code(400);
+    exit;
+}
+
+echo gethostbyaddr($_GET['ip']);
--- gethostbyname.php
+++ gethostbyname.php
@@ -1,1 +1,11 @@
+<?php
+header('X-Accel-Buffering: no');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: text/plain');
 
+if (!isset($_GET['hostname'])) {
+    http_response_code(400);
+    exit;
+}
+
+echo gethostbyname($_GET['hostname']);
