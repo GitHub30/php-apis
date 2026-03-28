aaf50923d8287e3b3abcb49759b30b6298949153
GitHub30 <funaox@gmail.com>
2026-03-28 07:03:16 +0000

    fix: Remove unnecessary Content-Type header from WHOIS response

--- whois.php
+++ whois.php
@@ -3,7 +3,6 @@ header('X-Accel-Buffering: no');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
-header('Content-Type: text/plain; charset=utf-8');
 
 $domain = $_GET['domain'] ?? '';
 
