bb9ae0aa7c70f41340ae59b8f689422113ed32c0
GitHub30 <funaox@gmail.com>
2026-03-29 12:09:02 +0000

    fix: Correct HTTP method handling and response codes in ftp.php

--- ftp.php
+++ ftp.php
@@ -10,11 +10,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] !== 'PUT') {
-    http_response_code(405);
-    exit;
-}
-
 function guessHostFromUsername($username)
 {
     $parts = explode('@', $username, 2);
@@ -35,9 +30,33 @@ if (!ftp_login($ftp, $_GET['username'], $_GET['password'])) {
     exit;
 }
 
-if (!ftp_put($ftp, $_GET['filename'], 'php://input')) {
-    http_response_code(500);
-    exit;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['filename'])) {
+        $contentType = mime_content_type($_GET['filename']);
+        if ($contentType) {
+            header("Content-Type: $contentType");
+        }
+        if (!ftp_get($ftp, 'php://output', $_GET['filename'])) {
+            http_response_code(404);
+        }
+    } else {
+        $files = ftp_nlist($ftp, $_GET['path'] ?? '.');
+        if (!$files) {
+            header('Content-Type: application/json');
+            echo json_encode($files, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+    }
+} else if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT'])) {
+    if (!ftp_put($ftp, $_GET['filename'], 'php://input')) {
+        http_response_code(500);
+        exit;
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    if (!ftp_delete($ftp, $_GET['filename'])) {
+        http_response_code(500);
+    }
+} else {
+    http_response_code(405);
 }
 
 ftp_close($ftp);
