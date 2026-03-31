eefd9f0a78fb4056adb3832072bfb837099e5ef9
GitHub30 <funaox@gmail.com>
2026-04-01 00:43:35 +0900

    Create radb.php

---
+++ radb.php
@@ -0,0 +1,18 @@
+<?php
+header('X-Accel-Buffering: no');
+header('Access-Control-Allow-Origin: *');
+
+$ip = $_GET['ip'] ?? '';
+
+if (filter_var($ip, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)) {
+    $ip = gethostbyname($ip);
+}
+
+if (!$ip || !filter_var($ip, FILTER_VALIDATE_IP)) {
+    http_response_code(400);
+    exit;
+}
+
+header('Content-Type: text/plain');
+$escaped = escapeshellarg($ip);
+passthru("whois -h whois.radb.net $escaped");
